7c17a86dbd2ad09d39d2c020bde5db82ec0d74f1
ade ade <ochobase@outlook.jp>
2025-05-23 16:08:21 +0900

    cnp

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 (async () => {
   const delayTime = 100;
-  let selects = [];
+  let selects;
   while (selects.length === 0) {
     selects = document.querySelectorAll(
       ".css-175oi2r.r-6koalj.r-eqz5dr.r-16y2uox.r-1habvwh.r-cnw61z.r-13qz1uu.r-1ny4l3l.r-1loqt21"
